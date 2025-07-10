7a89868c93c16659276486486bd38e894027bb5e
Özgür Uçar <ucar.ozgur@outlook.com>
2025-07-10 15:31:48 +0300

    React Project Created, Demo Speech Recognition Added

---
+++ speech-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SpeechDemo from './components/SpeechDemo';
+
+function App() {
+  return (
+    <div className='App'>
+      <SpeechDemo></SpeechDemo>
+    </div>
+  );
+}
+
+export default App;
